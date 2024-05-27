f607eca5e5a1cb4a0d54b0b8a2c6ad6ac4ea810b
imkaga <62307292+imkaga@users.noreply.github.com>
2024-05-27 20:15:54 +0200

    Update ApiKeys.js

--- src/components/ApiKeys.js
+++ src/components/ApiKeys.js
@@ -2,5 +2,4 @@ export const openWeatherApiKey = "85f93b7e11258cb2617c3f745ecf3349"; // OpenWeat
 export const openWeatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="; // OpenWeather URL
 export const spotifyClientId = '44c1aa3e0f954b9491bb515284729f0b'; // Spotify Client ID
 export const spotifyClientSecret = '343770fe2dcc4167acd9722f8c3424d9'; // Spotify Client Secret
-export const spotifyRedirectUri = ['http://localhost:3000/', 'https://spot-the-weather.vercel.app/'];
-const selectedRedirectUri = spotifyRedirectUri[0]; // You can choose any URI from the array based on your application's logic
+export const spotifyRedirectUri = 'https://spot-the-weather.vercel.app/';
